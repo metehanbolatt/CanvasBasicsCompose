cc9c90d9e2f578eab0a5856fb9193329c0fc102e
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-07-06 18:15:55 +0300

    drawRect

--- app/src/main/java/com/metehanbolat/canvasbasicscompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/canvasbasicscompose/MainActivity.kt
@@ -3,13 +3,51 @@ package com.metehanbolat.canvasbasicscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.metehanbolat.canvasbasicscompose.ui.theme.CanvasBasicsComposeTheme
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            MyCanvas()
         }
     }
+}
+
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(100f, 100f),
+            size = Size(100f, 100f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MyCanvasPrev() {
+    MyCanvas()
 }
