a47fc9949fed2b4970179605d30c2191ba253d4b
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-07-06 17:55:40 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/canvasbasicscompose/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.metehanbolat.canvasbasicscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.metehanbolat.canvasbasicscompose.ui.theme.CanvasBasicsComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            
+        }
+    }
+}
